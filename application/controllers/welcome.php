c5fc52e3f7eada619995ba2b946fe2b21d518c4f
Daniel Torscho <daniel.torscho@gmail.com>
2014-05-04 20:12:56 +0200

    Create welcome.php

---
+++ application/controllers/welcome.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	public function index()
+	{
+		$this->template->load();
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
