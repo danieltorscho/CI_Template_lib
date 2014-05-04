3d4ec288e45efb184918c4b59257a5346600fff5
Daniel Torscho <daniel.torscho@gmail.com>
2014-05-04 18:03:51 +0200

    Create template.php

---
+++ application/config/template.php
@@ -0,0 +1,65 @@
+<?php defined( 'BASEPATH' ) OR exit( 'No direct script access allowed' );
+/*
+| -------------------------------------------------------------------
+|  Global website settings
+| -------------------------------------------------------------------
+*/
+$config['tpl_sitename']			= 'Website name';
+
+/*
+|--------------------------------------------------------------------------
+| Default master layout
+|--------------------------------------------------------------------------
+| 'template/default'	Produces app/views/template/default.php
+*/
+$config['tpl_default']			= 'template/default';
+
+/*
+|--------------------------------------------------------------------------
+| Default css files
+|--------------------------------------------------------------------------
+| Array key				Filename of the stylesheet file
+| Array value			Type of provided stylesheet: all / screen / print
+*/
+$config['tpl_default_css']		= array(
+	'template.css'		=> 'all'
+);
+
+/*
+|--------------------------------------------------------------------------
+| Default javascript files
+|--------------------------------------------------------------------------
+| 'TRUE'				Inserts a javascript file into the HEADER section
+| 'FALSE'	(default)	Inserts a javascript file into the FOOTER section
+*/
+$config['tpl_default_js']		= array(
+	'appstrap.min.js'	=> TRUE,
+	'template.js'		=> FALSE
+);
+
+/*
+|--------------------------------------------------------------------------
+| Title separator
+|--------------------------------------------------------------------------
+*/
+$config['tpl_title_separator']	= ' | ';
+
+/*
+|--------------------------------------------------------------------------
+| Default folder structure
+|--------------------------------------------------------------------------
+*/
+$config['tpl_dir_assets']		= 'assets/';
+$config['tpl_dir_avatars']		= 'images/avatars/';
+$config['tpl_dir_products']		= 'images/products/';
+$config['tpl_dir_products_l']	= 'images/products/large/';
+$config['tpl_dir_products_m']	= 'images/products/medium/';
+$config['tpl_dir_products_s']	= 'images/products/small/';
+$config['tpl_dir_products_t']	= 'images/products/thumbs/';
+$config['tpl_dir_uploads']		= 'uploads/';
+
+
+
+
+/* End of file template.php */
+/* Location: ./application/config/template.php */
